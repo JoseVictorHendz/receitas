f7210b7fe1f44e446301426ffc8b6bcbc7b08639
Jose Victor Hendz Martins <Jose>
2018-12-01 07:45:50 -0200

    commit

--- app/src/main/java/com/example/jose_victor/receitas/MainActivity.kt
+++ app/src/main/java/com/example/jose_victor/receitas/MainActivity.kt
@@ -4,30 +4,41 @@ import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Spinner
-import android.widget.ArrayAdapter
+import android.widget.*
+import android.widget.AdapterView
 
 
 
 
-
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val spinner = findViewById<View>(R.id.instantaneas)  as Spinner
+        val spinner = findViewById<View>(R.id.categorias)  as Spinner
         createAdapterCategorias(spinner)
 
     }
 
     fun createAdapterCategorias(spinner: Spinner) {
         val adapter = ArrayAdapter.createFromResource(this,
-                R.array.instantaneas_array, android.R.layout.simple_spinner_item)
+                R.array.categorias_array, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         spinner.adapter = adapter
+        spinner.onItemSelectedListener = this
+
     }
+
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View,
+                                pos: Int, id: Long) {
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+    }
+
 }
